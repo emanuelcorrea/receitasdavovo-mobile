b94a27902109987450ca70003172a12054a9baf7
Emanuel Correa <emanuel.silvati@gmail.com>
2020-01-28 15:53:30 -0300

    Receitas page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:receitas_vovo/views/homePage.dart';
 import 'package:receitas_vovo/views/painelPage.dart';
 import 'package:receitas_vovo/views/AddReceitaPage.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  
+  runApp(MyApp());
+}
 
 var rotas = <String, WidgetBuilder> {
   '/' : (context) => HomePage(),
   '/painel' : (context) => PainelPage(),
   '/addReceita' : (context) => AddReceitaPage(),
 };
+
 
 class MyApp extends StatelessWidget {
   @override
