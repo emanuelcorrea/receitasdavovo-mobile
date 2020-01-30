dc586c5a76f7f613d8eb888d65381d3e17e4d39d
Emanuel Corrêa <emanuel.silvati@gmail.com>
2020-01-29 23:28:07 -0300

    Avaiable

--- lib/views/receitaPage.dart
+++ lib/views/receitaPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:receitas_vovo/models/Receita.dart';
 import 'dart:core';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -105,9 +104,34 @@ class _ReceitaPageState extends State<ReceitaPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        elevation: 0.0,
+        flexibleSpace: Image(
+          image: AssetImage('assets/images/bg2.png'),
+          fit: BoxFit.cover,
+        ),
+        title: Text('Receita'),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10.0, 9.0, 10.0, 9.0),
+            child: Container(
+              width: 39.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                  bottomRight: Radius.circular(50.0),
+                  bottomLeft: Radius.circular(50.0),
+                ),
+              ),
+              child: IconButton(
+                icon: Icon(FontAwesomeIcons.heart, color: loved ? Colors.grey : Color.fromRGBO(252, 152, 152, 1)),
+                onPressed: () {},
+                iconSize: 20.0,
+              ),
+            ),
+          ),
+        ],
       ),
-<<<<<<< HEAD
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -142,149 +166,121 @@ class _ReceitaPageState extends State<ReceitaPage> {
                 ),
               ],
             ),
-            child: Image.asset(
-              "assets/images/receitas/${widget.receita.img}",
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0, 0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        transform: Matrix4.translationValues(0, 25, -10),
-                        width: 50.0,
-                        child: Divider(color: Colors.black12,)
-                      ),
-                      Container(
-                        transform: Matrix4.translationValues(0, 25, -10),
-                        width: 50.0,
-                        child: Divider(color: Colors.black12)
-                      ),
-                    ],
+                  Container(
+                    height: 50.0,
+                    child: Text("${widget.receita.nome}".toUpperCase(), style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300),)
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0, 0.0),
+                ],
+              )
+            ),
+            Container(
+              transform: Matrix4.translationValues(0, -20.0, 0),
+              height: 5.0,
+              width: 100.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50.0),
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                  bottomRight: Radius.circular(50.0),
+                ),
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(252, 152, 152, 1),
+                    Colors.pinkAccent,
+                    Color.fromRGBO(252, 152, 152, 1),
+                  ]
+                )
+              ),
+              child: Container(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    height: 50.0,
+                    width: 250.0,
+                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 16.0),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(210, 210, 210, .2),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(0.0),
+                        bottomLeft: Radius.circular(0.0),
+                      ),
+                    ),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
                         Container(
-                          height: 50.0,
-                          child: Text("${widget.receita.nome}".toUpperCase(), style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300),)
+                          transform: Matrix4.translationValues(0, -5, 0),
+                          child: Icon(FontAwesomeIcons.clipboardList, color: Color.fromRGBO(252, 152, 152, 1),)
                         ),
+                        Text(' Ingredientes'.toUpperCase(), style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w300, fontSize: 17.0)),
                       ],
                     )
                   ),
                   Container(
-                    transform: Matrix4.translationValues(0, -20.0, 0),
-                    height: 5.0,
                     width: 100.0,
                     decoration: BoxDecoration(
+                      color: Color.fromRGBO(195, 195, 195, .1),
                       borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(50.0),
-                        topLeft: Radius.circular(50.0),
-                        topRight: Radius.circular(50.0),
-                        bottomRight: Radius.circular(50.0),
+                        bottomLeft: Radius.circular(5.0),
+                        bottomRight: Radius.circular(5.0),
                       ),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromRGBO(252, 152, 152, 1),
-                          Colors.pinkAccent,
-                          Color.fromRGBO(252, 152, 152, 1),
-                        ]
-                      )
                     ),
-                    child: Container(),
+                    padding: EdgeInsets.all(10.0),
+                    child: listaIngredientes(widget.receita.ingredientes)
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                  SizedBox(height: 20.0),
+                  Container(
+                    height: 50.0,
+                    width: 250.0,
+                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 16.0),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(210, 210, 210, .2),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(0.0),
+                        bottomLeft: Radius.circular(0.0),
+                      ),
+                    ),
+                    child: Row(
                       children: <Widget>[
                         Container(
-                          height: 50.0,
-                          width: 250.0,
-                          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 16.0),
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(210, 210, 210, .2),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10.0),
-                              topRight: Radius.circular(10.0),
-                              bottomRight: Radius.circular(0.0),
-                              bottomLeft: Radius.circular(0.0),
-                            ),
-                          ),
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                transform: Matrix4.translationValues(0, -5, 0),
-                                child: Icon(FontAwesomeIcons.clipboardList, color: Color.fromRGBO(252, 152, 152, 1),)
-                              ),
-                              Text(' Ingredientes'.toUpperCase(), style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w300, fontSize: 17.0)),
-                            ],
-                          )
+                          transform: Matrix4.translationValues(-5, -5, 0),
+                          child: Icon(FontAwesomeIcons.clipboardList, color: Color.fromRGBO(252, 152, 152, 1),)
                         ),
-                        Container(
-                          width: 100.0,
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(195, 195, 195, .1),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(5.0),
-                              bottomRight: Radius.circular(5.0),
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10.0),
-                          child: listaIngredientes(widget.receita.ingredientes)
-                        ),
-                        SizedBox(height: 20.0),
-                        Container(
-                          height: 50.0,
-                          width: 250.0,
-                          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 16.0),
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(210, 210, 210, .2),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10.0),
-                              topRight: Radius.circular(10.0),
-                              bottomRight: Radius.circular(0.0),
-                              bottomLeft: Radius.circular(0.0),
-                            ),
-                          ),
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                transform: Matrix4.translationValues(-5, -5, 0),
-                                child: Icon(FontAwesomeIcons.clipboardList, color: Color.fromRGBO(252, 152, 152, 1),)
-                              ),
-                              Text('Modo de preparo'.toUpperCase(), style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w300, fontSize: 17.0)),
-                            ],
-                          )
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(195, 195, 195, .1),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(5.0),
-                              bottomRight: Radius.circular(5.0),
-                            )
-                          ),
-                          padding: EdgeInsets.all(10.0),
-                          child: listaPreparo(widget.receita.preparo),
-                        ),
-                        SizedBox(height: 20.0),
+                        Text('Modo de preparo'.toUpperCase(), style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w300, fontSize: 17.0)),
                       ],
-                    ),
+                    )
                   ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(195, 195, 195, .1),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(5.0),
+                        bottomRight: Radius.circular(5.0),
+                      )
+                    ),
+                    padding: EdgeInsets.all(10.0),
+                    child: listaPreparo(widget.receita.preparo),
+                  ),
+                  SizedBox(height: 20.0),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       )
     );
   }
